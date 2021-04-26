2c80789291a800c19a8f9481db97e88312ea2cec
Dmitry Shipunov <vooxil@gmail.com>
2021-04-26 18:13:35 +0300

    update default message

--- function.go
+++ function.go
@@ -19,7 +19,7 @@ func HelloWorld(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	if d.Message == "" {
-		fmt.Fprint(w, "Hello World!")
+		fmt.Fprint(w, "No message provided!")
 		return
 	}
 	fmt.Fprint(w, html.EscapeString(d.Message))
