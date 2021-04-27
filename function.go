303f4aaba85b74e9bfd06b1e65ae9dcb57a89d89
Dmitry Shipunov <vooxil@gmail.com>
2021-04-28 01:13:43 +0300

    new function

--- function.go
+++ function.go
@@ -12,10 +12,12 @@ func GetMessage(w http.ResponseWriter, r *http.Request) {
 	var d struct {
 		Message string `json:"message"`
 	}
+
 	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
 		fmt.Fprint(w, "no message provided!")
 		return
 	}
+
 	if d.Message == "" {
 		fmt.Fprint(w, "empty message provided!")
 		return
